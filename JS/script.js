d8b7f93c7bd186f20b51d16ef79eacd24fa02423
Nicholas Hartman <hartn001@gmail.com>
2020-10-18 22:51:31 +0200

    Need to be better with this!

--- JS/script.js
+++ JS/script.js
@@ -83,3 +83,122 @@ function showSlides(n) {
 //   slides[slideIndex-1].style.display = "block";
 //   setTimeout(showSlides, 5000); // Change image every 5 seconds
 // }
+
+
+
+// *********************************** document scroll arrows **********************
+
+// Reference counter outside of function
+var idx = 0;
+// Collect all sections in a NodeList
+var sxn = document.querySelectorAll('section');
+// UPDATED
+/* Loop through the NodeList sxn
+|| Assign a data-id attribute to each section
+|| Set data-id value to current index of each
+|| section
+*/
+for (let i = 0; i < sxn.length; i++) {
+  sxn[i].setAttribute('data-id', i);
+}
+// Reference nav
+var nav = document.querySelector('arrow');
+
+// Collect all anchors into a HTMLCollection
+var lnx = document.links;
+// UPDATED
+// Register document on click event callback is move()
+document.getElementById("arrow").addEventListener('click', move, false);
+// UPDATED
+/* move() determines the direction of scroll by idx
+|| If a section is clicked instead of the arrows,
+|| then the data-id value of said section is now idx.
+|| So when a section is clicked, nothing happens until an
+|| arrow is clicked. Once that happens, scrolling starts
+|| from the last section clicked.
+*/
+function move(e) {
+  if (e.target == lnx[0]) {
+    idx--;
+    if (idx < 0) {
+      idx = sxn.length - 1;
+    }
+  } else if (e.target.tagName === 'SECTION') {
+    idx = e.target.getAttribute('data-id');
+  } else {
+    idx++;
+    if (idx > sxn.length - 1) {
+      idx = 0;
+    }
+  }
+  return idxScroll(idx);
+}
+
+// Pass idx thru idxScroll
+function idxScroll(idx) {
+  // Reference current active section
+  var act = document.querySelector('.active');
+  // Determine which section becomes active
+  var x = sxn[idx];
+  // Remove active class from current section
+  act.classList.remove('active');
+  // Add active class to new section
+  x.classList.add('active');
+  /* scrollIntoView method has a behavior option that animates
+  || scrolling
+  */
+  x.scrollIntoView({
+    behavior: 'smooth'
+  });
+}
+
+// ********************************************go to top button**************************************
+
+//Get the button:
+mybutton = document.getElementById("goToTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
+// **************************************drop down content****************************
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+// Close the dropdown menu if the user clicks outside of it
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
