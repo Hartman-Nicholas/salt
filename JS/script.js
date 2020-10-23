835bd11551384c8f2ea0672172f91386a53de680
Nicholas Hartman <hartn001@gmail.com>
2020-10-23 22:46:13 +0200

    Fixing CSS Styling

--- JS/script.js
+++ JS/script.js
@@ -62,10 +62,10 @@ function showSlides(n) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" actives", "");
   }
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+  dots[slideIndex - 1].className += " actives";
 }
 
 // var slideIndex = 0;
