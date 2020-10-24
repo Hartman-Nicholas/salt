3548a807ce30b1b4c0702a1ac23b3222e84198fe
Nicholas Hartman <hartn001@gmail.com>
2020-10-24 10:39:45 +0200

    Code clean up

--- JS/script.js
+++ JS/script.js
@@ -1,91 +1,29 @@
 /*jshint esversion: 6 */
 
-// Get the modal
-let modal = document.getElementsByClassName("myModal");
+// ******************************** Drop down content **************************
 
-// Get the button that opens the modal
-let btn = document.getElementsByClassName("myBtn");
-
-// Get the <button> element that closes the modal
-let button = document.getElementsByClassName("close");
-
-// When the user clicks the button, open the modal
-
-for (let i = 0; i < btn.length; i++) {
-  btn[i].addEventListener("click", function() {
-    modal[i].style.display = "block";
-    return modal[i];
-  });
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// When the user clicks on <button> (x), close the modal
-for (let i = 0; i < button.length; i++) {
-  button[i].addEventListener("click", function() {
-    modal[i].style.display = "none";
-  });
-}
+// Close the dropdown menu if the user clicks outside of it
 
-// When the user clicks anywhere outside of the modal, close it
-for (let i = 0; i < modal.length; i++)
-  window.addEventListener("click", function(event) {
-    if (event.target == modal[i]) {
-      modal[i].style.display = "none";
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
     }
-  });
-
-// *********************************slideshow**************************
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
   }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" actives", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " actives";
-}
+};
 
-// var slideIndex = 0;
-// showSlides();
-//
-// function showSlides() {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1;}
-//   slides[slideIndex-1].style.display = "block";
-//   setTimeout(showSlides, 4000); // Change image every 4 seconds
-// }
-
-
-
-// *********************************** document scroll arrows **********************
+// ******************************* Document scroll arrows **********************
 
 // Reference counter outside of function
 var idx = 0;
@@ -151,7 +89,7 @@ function idxScroll(idx) {
   });
 }
 
-// ********************************************go to top button**************************************
+// ********************************Go to top button*****************************
 
 //Get the button:
 let mybutton = document.getElementById("goToTop");
@@ -176,29 +114,87 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+// ******************************* Modal ***************************************
 
+// Get the modal
+let modal = document.getElementsByClassName("myModal");
 
-// **************************************drop down content****************************
+// Get the button that opens the modal
+let btn = document.getElementsByClassName("myBtn");
 
+// Get the <button> element that closes the modal
+let button = document.getElementsByClassName("close");
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+// When the user clicks the button, open the modal
+
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", function() {
+    modal[i].style.display = "block";
+    return modal[i];
+  });
 }
 
+// When the user clicks on <button> (x), close the modal
+for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", function() {
+    modal[i].style.display = "none";
+  });
+}
 
-// Close the dropdown menu if the user clicks outside of it
-
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+// When the user clicks anywhere outside of the modal, close it
+for (let i = 0; i < modal.length; i++)
+  window.addEventListener("click", function(event) {
+    if (event.target == modal[i]) {
+      modal[i].style.display = "none";
     }
+  });
+
+// ********************************* Slideshow *********************************
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
   }
-};
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" actives", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " actives";
+}
+
+// var slideIndex = 0;
+// showSlides();
+//
+// function showSlides() {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1;}
+//   slides[slideIndex-1].style.display = "block";
+//   setTimeout(showSlides, 4000); // Change image every 4 seconds
+// }
