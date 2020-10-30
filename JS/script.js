1cfe3726ba43da38ef2839a6d75fa1346ae21286
Nicholas Hartman <hartn001@gmail.com>
2020-10-30 10:26:41 +0100

    Update for opening on phone

--- JS/script.js
+++ JS/script.js
@@ -27,69 +27,69 @@ window.onclick = function (event) {
 
 // ******************************* Document scroll arrows **********************
 
-// Reference counter outside of function
-var idx = 0;
-// Collect all sections in a NodeList
-var sxn = document.querySelectorAll('section');
-// UPDATED
-/* Loop through the NodeList sxn
-|| Assign a data-id attribute to each section
-|| Set data-id value to current index of each
-|| section
-*/
-for (let i = 0; i < sxn.length; i++) {
-  sxn[i].setAttribute('data-id', i);
-}
-// Reference nav
-var nav = document.querySelector('arrow');
+// // Reference counter outside of function
+// var idx = 0;
+// // Collect all sections in a NodeList
+// var sxn = document.querySelectorAll('section');
+// // UPDATED
+// /* Loop through the NodeList sxn
+// || Assign a data-id attribute to each section
+// || Set data-id value to current index of each
+// || section
+// */
+// for (let i = 0; i < sxn.length; i++) {
+//   sxn[i].setAttribute('data-id', i);
+// }
+// // Reference nav
+// var nav = document.querySelector('arrow');
 
-// Collect all anchors into a HTMLCollection
-var lnx = document.links;
-// UPDATED
-// Register document on click event callback is move()
-document.getElementById("arrow").addEventListener('click', move, false);
-// UPDATED
-/* move() determines the direction of scroll by idx
-|| If a section is clicked instead of the arrows,
-|| then the data-id value of said section is now idx.
-|| So when a section is clicked, nothing happens until an
-|| arrow is clicked. Once that happens, scrolling starts
-|| from the last section clicked.
-*/
-function move(e) {
-  if (e.target == lnx[0]) {
-    idx--;
-    if (idx < 0) {
-      idx = sxn.length - 1;
-    }
-  } else if (e.target.tagName === 'SECTION') {
-    idx = e.target.getAttribute('data-id');
-  } else {
-    idx++;
-    if (idx > sxn.length - 1) {
-      idx = 0;
-    }
-  }
-  return idxScroll(idx);
-}
+// // Collect all anchors into a HTMLCollection
+// var lnx = document.links;
+// // UPDATED
+// // Register document on click event callback is move()
+// document.getElementById("arrow").addEventListener('click', move, false);
+// // UPDATED
+// /* move() determines the direction of scroll by idx
+// || If a section is clicked instead of the arrows,
+// || then the data-id value of said section is now idx.
+// || So when a section is clicked, nothing happens until an
+// || arrow is clicked. Once that happens, scrolling starts
+// || from the last section clicked.
+// */
+// function move(e) {
+//   if (e.target == lnx[0]) {
+//     idx--;
+//     if (idx < 0) {
+//       idx = sxn.length - 1;
+//     }
+//   } else if (e.target.tagName === 'SECTION') {
+//     idx = e.target.getAttribute('data-id');
+//   } else {
+//     idx++;
+//     if (idx > sxn.length - 1) {
+//       idx = 0;
+//     }
+//   }
+//   return idxScroll(idx);
+// }
 
-// Pass idx thru idxScroll
-function idxScroll(idx) {
-  // Reference current active section
-  var act = document.querySelector('.active');
-  // Determine which section becomes active
-  var x = sxn[idx];
-  // Remove active class from current section
-  act.classList.remove('active');
-  // Add active class to new section
-  x.classList.add('active');
-  /* scrollIntoView method has a behavior option that animates
-  || scrolling
-  */
-  x.scrollIntoView({
-    behavior: 'smooth'
-  });
-}
+// // Pass idx thru idxScroll
+// function idxScroll(idx) {
+//   // Reference current active section
+//   var act = document.querySelector('.active');
+//   // Determine which section becomes active
+//   var x = sxn[idx];
+//   // Remove active class from current section
+//   act.classList.remove('active');
+//   // Add active class to new section
+//   x.classList.add('active');
+//   /* scrollIntoView method has a behavior option that animates
+//   || scrolling
+//   */
+//   x.scrollIntoView({
+//     behavior: 'smooth'
+//   });
+// }
 
 // ********************************Go to top button*****************************
 
